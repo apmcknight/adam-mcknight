28d1e0be2bb794846a3c79ff9e30cffd3317a038
Adam McKnight <adammcknight@me.com>
2020-11-25 11:48:48 -0500

    Updated home content css.

--- src/components/homeContent.js
+++ src/components/homeContent.js
@@ -10,23 +10,26 @@ export default function HomeContent() {
   <section>
     <div class="relative bg-white overflow-hidden">
       <div class="flex justify-center max-w-10xl mx-auto ">
-        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-4xl lg:w-full lg:pb-28 xl:pb-32">
+        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           
           <h1 class="mt-32 ml-9 mb-10" style={{color: "black", fontSize: "3em", letterSpacing: "9px", fontWeight: "bold"}} dangerouslySetInnerHTML={{__html: homeData.home_header}}></h1>
 
-          <main class="flex flex-wrap-justify-center mx-auto max-w-5xl px-10 sm:px-6 lg:px-8">
+          <main class="flex flex-wrap-justify-left mx-auto sm:max-w-2xl lg:max-w-5xl px-10 sm:px-6 lg:px-8">
             
-            <div class="mr-10 sm:text-center lg:text-center">
+            <div class="lg:mr-10 sm:mr-0 sm:mb-10 sm:text-left lg:text-center">
               <p class="text-base text-gray-500 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0 text-justify">
                 <div dangerouslySetInnerHTML={{__html: homeData.home_content}}></div>
               </p>
-            </div>
-            
-            <div class="mr-10 sm:text-center lg:text-center">
-              <p class="text-base text-gray-500 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0 text-justify">
+              <p class="mt-7 text-base text-gray-500 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0 text-justify">
                  <div dangerouslySetInnerHTML={{__html: homeData.home_content_1}}></div>
               </p>
             </div>
+{/*             
+            <div class="md:mp-10 mr-10 sm:text-center lg:text-center">
+              <p class="text-base text-gray-500 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0 text-justify">
+                 <div dangerouslySetInnerHTML={{__html: homeData.home_content_1}}></div>
+              </p>
+            </div> */}
           </main>
 
           
