3a5c77e205a1ff8b410adccc5a2346fa3768823c
Adam McKnight <adammcknight@me.com>
2020-11-24 20:51:26 -0500

    Updated Privacy Notice Component.

--- src/components/privacyAlert.js
+++ src/components/privacyAlert.js
@@ -4,19 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle,faBell } from '@fortawesome/free-solid-svg-icons'
 
 export default function privacy(){
+
+    // function hide() {
+    //     style={display: "none;"}
+    //     console.log("working...")
+    // }
+
     return(
     <section class="fixed bottom-0 left-50 z-10">
     <div class="p-5">
         <div class="shadow-2xl flex items-center bg-gray-200 leading-none text-purple-600 rounded-full p-2 text-sm">
             <h1>
                 <FontAwesomeIcon class="inline-flex h-3 pl-3" icon={faBell} />
-                <span class="inline-flex rounded-full h-6 pl-2 justify-center items-center">PRIVACY NOTE &nbsp;&mdash;</span>
+                <span style={{letterSpacing: "3px", fontWeight: "bold"}} class="inline-flex rounded-full h-6 pl-2 justify-center items-center">PRIVACY NOTEICE &nbsp;&mdash;</span>
             </h1>
             <h2 class="flex px-2">
                 Read your Privacy Rights &amp; Our Terms
                 <Link class="pl-3 pr-3"to="/legal">here</Link>
             </h2>
             
+            {/* Figure out how to create a onClick event that will close the notification. React States is probably your best bet. */}
             <FontAwesomeIcon class="absolute right-4 top-5 w-5" icon={faTimesCircle} />
 
         </div>
