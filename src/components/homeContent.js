aa31e55f68beafb39e1ef1bf659538ac8060a504
Adam McKnight <adammcknight@me.com>
2020-11-25 10:36:52 -0500

    Updated Home Page schema.

--- src/components/homeContent.js
+++ src/components/homeContent.js
@@ -11,16 +11,28 @@ export default function HomeContent() {
     <div class="relative bg-white overflow-hidden">
       <div class="flex justify-center max-w-10xl mx-auto ">
         <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-4xl lg:w-full lg:pb-28 xl:pb-32">
-          <main class="flex flex-wrap-justify-center mt-10 mx-auto max-w-2xl px-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          
+          <h1 dangerouslySetInnerHTML={{__html: homeData.home_header}}></h1>
+
+          <main class="flex flex-wrap-justify-center mt-10 mx-auto max-w-5xl px-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div class="mr-10 sm:text-center lg:text-center">
               <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 text-justify">
                 <div dangerouslySetInnerHTML={{__html: homeData.home_content}}></div>
               </p>
             </div>
+            
+            <div class="mr-10 sm:text-center lg:text-center">
+              <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 text-justify">
+                <div dangerouslySetInnerHTML={{__html: homeData.home_content_1}}></div>
+              </p>
+            </div>
           </main>
+
+          
+
         </div>
-        </div>
-        </div>
+       </div>
+    </div>
        
   </section>
       
