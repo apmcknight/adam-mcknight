deb6f404c223592dcaf6a4edf4dad6fdbdf643e0
Adam McKnight <adammcknight@me.com>
2020-11-25 19:53:25 -0500

    Updated privacy alert component.

--- src/components/privacyAlert.js
+++ src/components/privacyAlert.js
@@ -12,18 +12,19 @@ export default function privacy(){
     // }
 
     return(
-    <section class="fixed bottom-0 left-50 z-10">
+    <section class="fixed bottom-0 ml-5 md:ml-0 lg:ml-0 z-10">
     <Fade triggerOnce duration={3000}>
     <div>
         <Slide triggerOnce duration={1000}>
             <div class="p-5">
-                <div style={{background: "#caae77", color: "#fff"}} class="shadow-2xl flex items-center leading-none text-purple-600 rounded-full p-2 text-sm">
+                <div style={{background: "#caae77", color: "#fff"}} class="shadow-2xl flex items-center leading-none sm:w-100 h-9 rounded-full sm:p-1 lg:p-2 text-sm">
                     <h1 style={{color: "#fff"}}>
-                        <FontAwesomeIcon class="inline-flex h-3 pl-3" icon={faBell} />
-                        <span style={{letterSpacing: "3px", fontWeight: "bold"}} class="inline-flex rounded-full h-6 pl-2 justify-center items-center">PRIVACY NOTICE&nbsp;&mdash;</span>
+                        <FontAwesomeIcon class="inline-flex h-3 ml-5 mr-2" icon={faBell} />
+                        
+                        <span style={{letterSpacing: "3px", fontWeight: "bold"}} class="inline-flex sm:h-3 lg:h-6 pl-2 justify-center items-center">PRIVACY NOTICE&nbsp;&mdash;</span>
                     </h1>
                     <h2 style={{color: "#fff"}} class="flex px-2">
-                        Read your Privacy Rights &amp; Our Terms
+                        Read our policies
                         <Link class="pl-3 pr-3"to="/legal">here</Link>
                     </h2>
                     
