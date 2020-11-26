b0452718611be7c666c8f319cc8308d92a077075
Adam McKnight <adammcknight@me.com>
2020-11-25 23:37:51 -0500

    Updated site content and stles.

--- src/components/BlogHome.js
+++ src/components/BlogHome.js
@@ -2,32 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import useBlogData from "../static_queries/useBlogData"
 import blogListStyles from "../styles/components/bloglist.module.scss"
-import Img from 'gatsby-image'
 
 export default function BlogList() {
   const blogData = useBlogData()
   function renderBlogData() {
     return (
-      <div>
+      <div class="w-7/12 h-auto ml-auto mr-auto mt-20">
         {blogData
           .filter(blog => blog.node.frontmatter.title !== "")
           .map(blog => {
             return (
-              <Link to={`/blog/${blog.node.fields.slug}`} key={blog.node.id}>
-                <li className={blogListStyles.li} key={blog.node.fields.slug}>
-                  <div className={blogListStyles.list__hero}>
-                    <Img 
-                      fluid={
-                        blog.node.frontmatter.hero_image.childImageSharp.fluid
-                      }
-                      alt={blog.node.frontmatter.title}
-                    />
-                  </div>
-                  <div className={blogListStyles.list__info}>
-                    <h2>{blog.node.frontmatter.title}</h2>
+              <Link class="blog-home-links" to={`/blog/${blog.node.fields.slug}`} key={blog.node.id}>
+                <li class="shadow-lg p-14 m-10 rounded-lg">
+                    <h1>{blog.node.frontmatter.title}</h1>
                     <h3>{blog.node.frontmatter.date}</h3>
                     <p>{blog.node.excerpt}</p>
-                  </div>
                 </li>
               </Link>
             )
