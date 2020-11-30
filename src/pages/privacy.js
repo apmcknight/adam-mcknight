e3937e9a0ebce876ea2b6ce897521df68c71a96a
Adam McKnight <adammcknight@me.com>
2020-11-29 21:26:50 -0500

    Updated and created new pages

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,13 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 import useSiteMetaData from "../static_queries/useSiteMetadata"
 import PrivacyAlertBar from "../components/privacyAlert"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Logo from "../../content/images/a-black.png"
 
-export default function SocialLinks() {
-  const { socialMediaLinkData } = useSiteMetaData()
+export default function License() {
+  const { privacyData } = useSiteMetaData()
   return (
     <main>
       <Header/>
@@ -15,13 +14,14 @@ export default function SocialLinks() {
       <section class="mt-20 mb-10">
         <div className="text-center align-content-center">
           <img class="h-auto w-20 ml-auto mr-auto mb-10" src={Logo} alt="Adam McKnight Typemark"/>
-            <h1 class="heading-text pl-24 pr-24 w-50%" dangerouslySetInnerHTML={{ __html: socialMediaLinkData.socialMediaLink_header}}></h1>
+            <h1 class="heading-text pl-24 pr-24 w-50%" dangerouslySetInnerHTML={{ __html: privacyData.privacy_header }}></h1>
+            <h3 class="heading-text pl-24 pr-24 w-50%" dangerouslySetInnerHTML={{ __html: privacyData.privacy_subheader }}></h3>
         </div>
       </section>
 
       <section class="mt-10 mb-32">
         <div className="text-justify align-content-center">
-          <Link target="_blank" to={socialMediaLinkData.socialMediaLink_link_0}  class="about-header mt-52 ml-9 mb-10 h-auto w-4/5 lg:w-2/4 ml-auto mr-auto" dangerouslySetInnerHTML={{__html: socialMediaLinkData.socialMediaLink_title_0}}></Link>
+          <p class="about-header mt-52 ml-9 mb-10 h-auto w-4/5 lg:w-2/4 ml-auto mr-auto" dangerouslySetInnerHTML={{__html: privacyData.privacy_content}}></p>
         </div>
       </section>
 
